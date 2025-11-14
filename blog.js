a82bb389808533b3b41eed64854dfb3f64e89e1b
Jim Kennedy <kennedyomata@gmail.com>
2025-11-14 14:34:43 +0300

    chore: initialize package.json for blog application with dependencies

---
+++ blog.js
@@ -0,0 +1,98 @@
+import express from "express";
+const app = express();
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+const port = 3000;
+import { users, blogs } from "./data.js";
+
+app.use(express.json());
+
+// API endpoints go here
+// USers API
+
+// Retruve all users
+app.get("/users", (req, res) => {
+  if (users.length === 0) {
+    return res.status(404).send("No users found.");
+  } else {
+    res.json(users);
+  }
+});
+
+// Retrieve a user by ID
+app.get("/users/:id", (req, res) => {
+  const user = users.find((u) => u.userId === parseInt(req.params.id));
+  if (!user) {
+    return res.status(404).send("The user with the given ID was not found.");
+  } else {
+    res.json(user);
+  }
+});
+
+// Create a new user
+app.post("/users", (req, res) => {
+  const newUser = {
+    userId: users.length > 0 ? Math.max(...users.map((u) => u.userId)) + 1 : 1,
+    name: req.body.name,
+  };
+  users.push(newUser);
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const dataFilePath = path.join(__dirname, "data.js");
+  const updatedData = `export let users = ${JSON.stringify(
+    users,
+    null,
+    2
+  )}; export let blogs = ${JSON.stringify(blogs, null, 2)};`;
+  //write the new data back to data.js
+  fs.writeFile(dataFilePath, updatedData, "utf8", (err) => {
+    if (err) {
+      console.error(err);
+      return res
+        .status(500)
+        .send("An error occurred while writing to the file.");
+    }
+    res.status(201).json(newUser);
+  });
+});
+
+// Update an existing user
+app.put("/users/:id", (req, res) => {
+  const user = users.find((u) => u.userId === parseInt(req.params.id));
+  if (!user)
+    return res.status(404).send("The user with the given ID was not found.");
+
+  user.name = req.body.name;
+  res.json(user);
+});
+
+// Delete a user
+app.delete("/users/:id", (req, res) => {
+  const userIndex = users.findIndex(
+    (u) => u.userId === parseInt(req.params.id)
+  );
+  if (userIndex === -1)
+    return res.status(404).send("The user with the given ID was not found.");
+
+  const deletedUSer = users.splice(userIndex, 1);
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const dataFilePath = path.join(__dirname, "data.js");
+  const updatedData = `export let users = ${JSON.stringify(users, null, 2)};
+
+export let blogs = ${JSON.stringify(blogs, null, 2)};`;
+
+  fs.writeFile(dataFilePath, updatedData, "utf8", (err) => {
+    if (err) {
+      console.error(err);
+      return res
+        .status(500)
+        .send("An error occurred while writing to the file.");
+    }
+    res.json(deletedUSer);
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
