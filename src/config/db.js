ae2c47e1d6a9d6a53c162898fa4ba4950d4706cd
Jim Kennedy <kennedyomata@gmail.com>
2025-11-29 14:49:28 +0300

    feat : added comments

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,23 @@
-// database configuration
-import mongoose  from "mongoose";
+// Import the Mongoose library for MongoDB object modeling
+import mongoose from "mongoose";
 
+// Define the MongoDB connection URI, using an environment variable or a default local URI
 const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/blogs-db";
 
-// connect to the database
+/**
+ * @description Asynchronously connects to the MongoDB database using the defined URI.
+ * Logs a success message on a successful connection or an error message and exits the process on a failed connection.
+ */
 async function DBconnect() {
-
   try {
-    await mongoose.connect(uri)
+    await mongoose.connect(uri);
     console.log("✅ Connected to MongoDB via Mongoose!");
   } catch (err) {
     console.error(`Error connecting to MongoDB: ${err.message}`);
+    // Exit the process with a failure code if the connection fails
     process.exit(1);
   }
 }
 
-//export the connection function
+// Export the DBconnect function to be used in other parts of the application
 export default DBconnect;
