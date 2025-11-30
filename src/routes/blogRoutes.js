6872ce5eb5c47b2c566d9241a10d9deb8548b64b
Jim Kennedy <kennedyomata@gmail.com>
2025-11-30 18:03:15 +0300

    feat:
    --added routes from the controller file

--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -5,5 +5,9 @@ const router = express.Router();
 // Route to create a new blog post
 router.post("/", blogsController.createBlog);
 router.get("/", blogsController.getBlogs);
+router.get("/:id", blogsController.getBlog);
+router.patch("/:id", blogsController.editBlog);
+router.put("/:id", blogsController.editBlog);
+router.delete("/:id", blogsController.deleteBlog);
 
 export default router;
