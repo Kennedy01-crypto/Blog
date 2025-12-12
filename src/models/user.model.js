37c05a05de6ed1a12432ed18fd72afe73eb47818
Jim Kennedy <kennedyomata@gmail.com>
2025-12-12 23:03:41 +0300

    feat(model) --added bcrypt for password hashing and jsonwebtoken for basic auth handling --addded a password field --added timestamps --presave -for password hashing -- comparePAssword to compare password when logging in -- createJWT to create JSON web tokens for authentication

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
@@ -23,7 +25,7 @@ const userSchema = new mongoose.Schema(
     email: {
       type: String,
       required: [true, "An email is required."],
-      unique: [true, "Email must be unique."],
+      unique: [true, "User with this email already exists"],
       lowercase: true,
       trim: true,
       match: [/^\S+@\S+\.\S+$/, "Please use a valid email address."],
@@ -32,11 +34,18 @@ const userSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [8, "Password must be lnger than 8 characters"],
+      select: false,
+    },
   },
   {
     // Ensure virtuals are included in toJSON and toObject outputs
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
+    timestamps: true,
   }
 );
 
@@ -44,6 +53,40 @@ const userSchema = new mongoose.Schema(
 userSchema.virtual("fullName").get(function () {
   return `${this.firstName} ${this.lastName}`;
 });
+
+//Presave function to hash passsword
+userSchema.pre("save", async function (next) {
+  //check if hte pasword has been modified
+  if (!this.isModified("password")) {
+    return next;
+  }
+  try {
+    //use a cost factor of 10
+    //the salt is generated ad included in the hasg automatically
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+//Method to compare candidate password with hashed password
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
+
+//Method to generate JWT
+//will be called on a user instance after succesfull login
+userSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userId: this._id, email: this.email, username: this.username }, //Payload: User ID, email and username
+    process.env.JWT_SECRET, //secret kay from env variables
+    {
+      expiresIn: process.env.JWT_LIFETIME, //Token expiration time
+    }
+  );
+};
 
 const User = mongoose.model("User", userSchema);
 
